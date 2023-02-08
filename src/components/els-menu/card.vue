38bf5d5ea948d3ddf6da68da554145852ea2dc7e
mike-work <evemike@163.com>
2023-02-08 19:59:49 +0800

    优化~

--- src/components/els-menu/card.vue
+++ src/components/els-menu/card.vue
@@ -4,36 +4,76 @@
       <slot name="left"></slot>
       <div class="_menus">
         <template v-for="(m, i) in menus" :key="`${m.key || ''}-${i}`">
-          <div class="_item"></div>
+          <div
+            class="_item"
+            :class="currentMenu == m ? 'is-active' : ''"
+            @mouseenter="() => handleOver(m)"
+            @mouseleave="() => handleOut(m)"
+            @click="() => {}"
+          >
+            <span class="_label text-18px font-normal">{{ m.label }}</span>
+            <b class="_bg absolute bottom-0 left-0 h-10px w-0"></b>
+          </div>
         </template>
       </div>
-
       <slot name="right"></slot>
     </div>
-    <div class="_card z-1">
-      <slot name="card"></slot>
+    <div
+      class="_card h-300px absolute z-1 w-100%"
+      :class="[currentMenu ? 'xyz-in' : 'xyz-out']"
+      xyz="fade up-100% duration-10 short-100% ease-liner"
+      @mouseenter="() => handleOver()"
+      @mouseleave="() => handleOut()"
+    >
+      <slot name="card" v-bind:scope="{ menu: currentMenu }"></slot>
     </div>
   </div>
 </template>
 
 <script>
-import {} from "vue"
+import {ref} from "vue"
+
+type Menu = Record<string,any> & {key?:string,label:string,path?:string,children?:any[]};
 
 interface Props {
-  menus:(Record<string,any> & {key?:string,label:string,path?:string,children?:any[]})[]
+  menus:Menu[]
 }
 
 const props = withDefaults(defineProps<Props>(),{
   menus:() => []
 })
+const currentMenu = ref<Menu>();
+let temp:Menu|undefined = undefined;
+//
+const handleOver = (m?:any) => {
+  if(m){
+    currentMenu.value = m;
+  }
+  else{
+    currentMenu.value = temp;
+  }
+}
+const handleOut = (m?:any) => {
+  currentMenu.value = undefined;
+  if(m){
+    temp = m;
+  }
+}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .els-menu._card {
   ._header {
     &:hover {
       backdrop-filter: blur(10px);
     }
+    ._menus {
+      ._item {
+      }
+    }
+  }
+  >._card{
+
   }
 }
 </style>
